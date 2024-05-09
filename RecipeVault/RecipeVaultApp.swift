0f8c608a5a39e8e97197d18b5fd4fb26f61caa5b
MC <MohamadNagib.Chrara@student.uts.edu.au>
2024-05-10 08:57:28 +1000

    Initial Commit

---
+++ RecipeVault/RecipeVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeVaultApp.swift
+//  RecipeVault
+//
+//  Created by SR71 on 10/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct RecipeVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
